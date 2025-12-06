b3c8b70e12dd339a39a71d99bf95fbf5cc546f9d
pierosantana <piero.santanalarosa@gmail.com>
2025-12-06 15:45:51 -0500

    fix: update Main.java

--- oop/library-system/src/Main.java
+++ oop/library-system/src/Main.java
@@ -1,6 +1,8 @@
 import model.Book;
 import model.Magazine;
 
+import java.sql.SQLOutput;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
@@ -9,19 +11,21 @@ public class Main {
         Book book = new Book("Isaac Asimov", "El fin de la eternidad", "9788497933537");
         Magazine magazine = new Magazine("Forbes", "Actualidad y Negocios", 67);
 
-        System.out.println("Books is loaned?: " + book.isLoaned());
-        System.out.println("Magazine is loaned?: " + magazine.isLoaned());
+        System.out.println("- Book: " + book.getAuthor() + "-" + book.getTitle());
+        System.out.println(":: Is book loaned?: " + book.isLoaned());
+        System.out.println("- Magazine: " + magazine.getAuthor() + "-" + magazine.getTitle());
+        System.out.println(":: Is magazine loaned?: " + magazine.isLoaned());
 
         book.loan();
         magazine.loan();
 
         System.out.println("---------------------------------");
-        System.out.println("Books is loaned?: " + book.isLoaned());
-        System.out.println("Magazine is loaned?: " + magazine.isLoaned());
+        System.out.println("Is book loaned?: " + book.isLoaned());
+        System.out.println("Is magazine loaned?: " + magazine.isLoaned());
 
         System.out.println("---------------------------------");
         System.out.println("Exception:");
         book.loan();
-        
+
     }
 }
